a3e1102be54dcaea1c6bcdeb036bf2bee8d3890e
Pedro Teles <pedrhenrique3027@gmail.com>
2025-05-19 16:00:23 -0300

    fix vercel error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from "@/generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof PrismaClient.prototype.$extends> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  }).$extends(withAccelerate());
+const extendedPrisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+}).$extends(withAccelerate());
+
+export const prisma = globalForPrisma.prisma ?? (extendedPrisma as unknown as PrismaClient);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
